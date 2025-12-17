ce15ad4602de3a06dbe41b5653b46674472dcfd1
Fazry <lhuqita.fazry@gmail.com>
2025-12-17 10:33:55 +0700

    Fixing bug

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -9,7 +9,7 @@ export default function AdminLogin() {
 
     const handleLogin = (e: React.FormEvent) => {
         e.preventDefault();
-        if (password === "admin123") {
+        if (password === "1ZCm>r3jDK7x") {
             // Simple demo auth
             localStorage.setItem("admin_auth", "true");
             navigate("/admin");
